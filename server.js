f118a3bba7191605eb7ca555563f26452752fdb0
sweet_jesus <ozanocak.ca@gmail.com>
2017-12-06 14:35:42 -0500

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿var http = require("http");
+var server = http.createServer(function (req, res) {
+    console.log(req.url);
+    //we can also write the respond in html page
+    res.write( "<html><body><h1>" +req.url+ "</h1></body></html>" );
+    //after req arrived, http will keep responding until we end it
+    res.end();
+});
+server.listen(3000);
+//whatever you request in port 3000 , it will print in cli 
+//ex: localhost:3000/thisis/fun  cli will print /thisis/fun
